5b714a388d674d9f8ee8349e464b2c88caa3d75f
Rishabh <rishabh.sharma3@appinventiv.com>
2025-06-27 17:31:38 +0530

    resolved conflict

--- src/features/manager/Login.tsx
+++ src/features/manager/Login.tsx
@@ -4,7 +4,7 @@ import { loginManager } from "../manager/auth/managerAuthService";
 
 export default function ManagerLoginPage() {
   const [formData, setFormData] = useState({
-    email: "",
+    email: "",git 
     password: "",
     rememberMe: false,
   });
